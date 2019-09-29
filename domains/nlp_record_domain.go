509c8897644c14a032761635f521d7fbc5d7e611
chanasit <kai.chr160@gmail.com>
2019-09-30 02:39:47 +0700

    create index keyword minhash

--- domains/nlp_record_domain.go
+++ domains/nlp_record_domain.go
@@ -9,7 +9,7 @@ type NlpRecordDomain struct {
 	gorm.Model
 	ShopID         uint
 	Keyword        string
-	KeywordMinhash uint32
+	KeywordMinhash uint32 `gorm:"index:idx_keyword_minhash"`
 	Intent         string
 }
 
