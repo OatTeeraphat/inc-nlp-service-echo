0b358435890c568a8f3ee84f4c9c41d9aae5dc20
oatteeraphat <oateerapat@gmail.com>
2019-11-01 01:51:10 +0700

    add footer login

--- public/presenters/dashboard_presenter.js
+++ public/presenters/dashboard_presenter.js
@@ -2,9 +2,15 @@ var dashboardPresenter = Vue.component('dashboard-presenter', {
 	template: `
 	<div class="warp">
 		<nav-presenter></nav-presenter>
-        <h1>Home</h1>
-        <p>This is home page</p>
-    </div>
+		<h1>Home</h1>
+		<p>This is home page</p>
+		<div class="row">
+			<div class="col-6">
+			<p>Bash Accuracy</p>
+			<line-chart></line-chart>
+			</div>
+		</div>
+	</div>
     `,
 	data: function () {
 		return {
