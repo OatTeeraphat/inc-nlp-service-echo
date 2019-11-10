a763f50b89c3232d7a846436bde229aa3c32cc53
Oat <Teeraphat@tencent.co.th>
2019-11-10 22:19:55 +0700

    add dropdown sign out

--- public/component/navbar/nav_component.js
+++ public/component/navbar/nav_component.js
@@ -2,11 +2,18 @@ var navPresenter = Vue.component('nav-component', {
 	template: `
 	<div class="fixed-top navbar-warpper bg-light">
 	
-		<nav class="navbar navbar-expand-lg navbar-dark container">
+		<nav class="navbar navbar-expand-lg navbar-dark container justify-content-between">
 			<a class="navbar-brand mr-auto mr-lg-0 visible" href="#"><img src="assets/logo-black.png" alt=""></a>
-			<button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
-				<span class="navbar-toggler-icon"></span>
-			</button>
+			<div class="btn-group">
+				<button type="button" class="btn btn-link dropdown-toggle dropdown-toggle-no-arrow" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					<i class="fe fe-menu"></i>
+				</button>
+				<div class="dropdown-menu dropdown-menu-right animated faster bounceIn">
+					<router-link class="dropdown-item" href="#" :to="{ path: '/setting'}">SETTING</router-link>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" @click="signOut">SIGN OUT</a>
+				</div>
+			</div>
 		</nav>
 		
 		<nav class="nav nav-underline nav-sub container bg-light">
@@ -17,7 +24,6 @@ var navPresenter = Vue.component('nav-component', {
 			<router-link class="nav-link" :to="{ path: '/story'}"> STORY </router-link>
 			<router-link class="nav-link" :to="{ path: '/logging'}"> LOGGING </router-link>
 			<router-link class="nav-link" :to="{ path: '/setting'}"> SETTING </router-link>
-			<a class="nav-link" @click="signOut"> SIGN OUT </a>
 		</nav>
 
 	</div>
