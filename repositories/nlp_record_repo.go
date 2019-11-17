ae39a33987498099a8378ab9313343df89844bc5
chanasit <kai.chr160@gmail.com>
2019-11-18 01:27:13 +0700

    limit 500 ++

--- repositories/nlp_record_repo.go
+++ repositories/nlp_record_repo.go
@@ -71,7 +71,7 @@ func (repo *NlpRecordRepository) Delete() *gorm.DB {
 // Count Count
 func (repo *NlpRecordRepository) Count() int64 {
 	var totalPage int64
-	repo.DB.Where(&domains.NlpRecordDomain{}).Count(&totalPage)
+	repo.DB.Table("nlp_records").Where(&domains.NlpRecordDomain{}).Count(&totalPage)
 	return totalPage
 }
 
