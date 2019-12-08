e6841ef80da617b82ab19ec53d71a406ab8ba497
Twatch <neungza_neung@hotmail.com>
2019-12-09 03:26:06 +0700

    merge

--- public/share-module/repositories/http_repository.js
+++ public/share-module/repositories/http_repository.js
@@ -2,7 +2,8 @@ export class HttpRepository {
 
     constructor(cookieRepo) {
         this.cookieRepo = cookieRepo
-        this.BASE_API = "http://localhost:9000"
+        // this.BASE_API = "http://localhost:9000"
+        this.BASE_API = "https://inc-nlp-service-echo.herokuapp.com"
         this.GATEWAY_API = "https://inc-auth.herokuapp.com"
     }
 
