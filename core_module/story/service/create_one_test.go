6d058df9c0bde2264be2dd3067cbae1636333bee
chanasit <kai.chr160@gmail.com>
2019-12-18 21:54:58 +0700

    stoy create one testcase ++

---
+++ core_module/story/service/create_one_test.go
@@ -0,0 +1,73 @@
+package service
+
+import (
+	"inc-nlp-service-echo/business_module/domains"
+	"inc-nlp-service-echo/business_module/repositories/mocks"
+	"inc-nlp-service-echo/core_module/story/dao"
+	"testing"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestServiceCreateOneStory(t *testing.T) {
+	assert := assert.New(t)
+
+	var TestCase = []struct {
+		Name            string
+		Input           string
+		Expected        string
+		MockStoryDAO    dao.NewStoryDao
+		MockStoryDomain domains.StoryDomain
+	}{
+		{
+			Name: "invalid case",
+			MockStoryDAO: dao.NewStoryDao{
+				AppID:       "00000000-0000-0000-0000-000000000000",
+				Name:        "name0",
+				Description: "desc0",
+			},
+			MockStoryDomain: domains.StoryDomain{
+				BaseDomain: domains.BaseDomain{
+					ID:        uuid.FromStringOrNil("00000000-0000-0000-0000-000000000000"),
+					CreatedAt: time.Date(0, 0, 0, 0, 0, 0, 0, time.UTC),
+					UpdatedAt: time.Date(0, 0, 0, 0, 0, 0, 0, time.UTC),
+				},
+				AppID:       uuid.FromStringOrNil("00000000-0000-0000-0000-000000000000"),
+				Name:        "name0",
+				Description: "desc0",
+			},
+			Expected: "invalid",
+		},
+		{
+			Name: "ok case",
+			MockStoryDAO: dao.NewStoryDao{
+				AppID:       "00000000-0000-0000-0000-000000000000",
+				Name:        "name0",
+				Description: "desc0",
+			},
+			MockStoryDomain: domains.StoryDomain{},
+			Expected:        "OK",
+		},
+	}
+
+	for _, test := range TestCase {
+		mockStoryRepo := &mocks.IStoryRepository{}
+		mockStoryRepo.On("FindByName", "name0").Return(test.MockStoryDomain)
+
+		var storyDomain domains.StoryDomain
+		storyDomain.Name = test.MockStoryDAO.Name
+		storyDomain.Description = test.MockStoryDAO.Description
+		storyDomain.AppID = test.MockStoryDomain.AppID
+
+		mockStoryRepo.On("Save", &storyDomain).Return(nil)
+
+		service := NewService(mockStoryRepo)
+
+		actual := service.CreateOneStory(test.MockStoryDAO)
+
+		assert.Equal(actual, test.Expected)
+	}
+
+}
